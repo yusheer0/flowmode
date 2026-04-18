9a8e989ed84c2ef882d18a876456f5f6447104cc
yusheero <yusheero.dev@gmail.com>
2026-04-19 01:07:03 +1000

    fix : v0.3.0

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,10 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
+import '@fontsource/bitter/400.css'
+import '@fontsource/bitter/500.css'
+import '@fontsource/bitter/600.css'
+import '@fontsource/bitter/700.css'
 import './styles/main.scss'
 
 const app = createApp(App)
